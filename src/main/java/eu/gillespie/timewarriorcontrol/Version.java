79bd9b95f446f392517f71f6bd4e87c2155a4e4d
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-05 00:39:33 +0200

    Add version comparison

--- src/main/java/eu/gillespie/timewarriorcontrol/Version.java
+++ src/main/java/eu/gillespie/timewarriorcontrol/Version.java
@@ -3,6 +3,8 @@ package eu.gillespie.timewarriorcontrol;
 import eu.gillespie.timewarriorcontrol.exception.VersionFormatException;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class Version {
 
@@ -33,5 +35,18 @@ public class Version {
     @Override
     public String toString() {
         return String.format("%d.%d.%d", getMajorVersion(), getMinorVersion(), getPatchVersion());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Version version = (Version) o;
+        return getMajorVersion() == version.getMajorVersion() && getMinorVersion() == version.getMinorVersion() && getPatchVersion() == version.getPatchVersion();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMajorVersion(), getMinorVersion(), getPatchVersion());
     }
 }
