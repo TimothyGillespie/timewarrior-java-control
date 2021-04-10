e912580ef1827f96bd27c40d72926bf65d7adbba
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-10 12:21:07 +0200

    Add test to assure working timewarrior version

---
+++ src/test/java/eu/gillespie/test/AssumptionTest.java
@@ -0,0 +1,46 @@
+package eu.gillespie.test;
+
+import eu.gillespie.timewarriorcontrol.TimeWarrior;
+import eu.gillespie.timewarriorcontrol.Version;
+import eu.gillespie.timewarriorcontrol.exception.VersionFormatException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+ * This class is supposed to test for assumptions we make during our tests.
+ */
+public class AssumptionTest {
+
+    @Test
+    void assumeCorrectTimeWarriorVersionRange() throws VersionFormatException, IOException {
+        Version runningVersion = new TimeWarrior().getVersion();
+
+        Version minimumVersion = new Version(1,4,2);
+        Version maximumVersionExclusive = new Version(2,0,0);
+
+        assertTrue(
+
+                runningVersion.compareTo(minimumVersion) >= 0,
+
+                String.format(
+                    "Running version was %s but must be at least %s.",
+                    runningVersion.toString(),
+                    minimumVersion.toString()
+                )
+        );
+
+        assertTrue(
+
+                runningVersion.compareTo(maximumVersionExclusive) < 0,
+
+                String.format(
+                    "Running version was %s but must lower than %s.",
+                    runningVersion.toString(),
+                    minimumVersion.toString()
+                )
+        );
+    }
+}
