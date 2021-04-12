3f9d0ede32abb91f069c87e31346ef39c47de9fe
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-12 17:34:49 +0200

    Make PermissionException a RunTime Exception

--- src/main/java/eu/gillespie/timewarriorcontrol/exception/PermissionException.java
+++ src/main/java/eu/gillespie/timewarriorcontrol/exception/PermissionException.java
@@ -2,7 +2,7 @@ package eu.gillespie.timewarriorcontrol.exception;
 
 import eu.gillespie.timewarriorcontrol.Permission;
 
-public class PermissionException extends Exception {
+public class PermissionException extends RuntimeException {
     public PermissionException(Permission permission) {
         super("The permission could not be assigned because it was disabled by the config key " +
                 permission.getConfigKey()
