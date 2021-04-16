161bb30119f29a07754400a527e9656e10ef8192
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-16 23:12:31 +0200

    Define equality for Tag

--- src/main/java/eu/gillespie/timewarriorcontrol/Tag.java
+++ src/main/java/eu/gillespie/timewarriorcontrol/Tag.java
@@ -3,11 +3,26 @@ package eu.gillespie.timewarriorcontrol;
 import lombok.Getter;
 import lombok.NonNull;
 
+import java.util.Objects;
+
 @Getter
 public class Tag {
     private final String name;
 
     public Tag(@NonNull String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(getName(), tag.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
