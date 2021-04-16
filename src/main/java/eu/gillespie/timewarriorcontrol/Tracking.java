fd189dc28ff24a7f1202a9d1b2eac5df884c15ba
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-16 21:07:32 +0200

    Add annotation and constructors to Tracking

--- src/main/java/eu/gillespie/timewarriorcontrol/Tracking.java
+++ src/main/java/eu/gillespie/timewarriorcontrol/Tracking.java
@@ -1,13 +1,23 @@
 package eu.gillespie.timewarriorcontrol;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@Setter
 public class Tracking {
+    String annotation;
     List<Tag> tags;
     LocalDateTime startTime;
     LocalDateTime endTime;
+
+    public Tracking(int id) {
+
+    }
+
 }
