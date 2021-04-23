791180ea01f78c306cb0c2fe0b4f5e3214515da3
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-23 02:07:57 +0200

    Improve test structure for BlockTest

--- src/test/java/eu/gillespie/test/terminal/BlockTest.java
+++ src/test/java/eu/gillespie/test/terminal/BlockTest.java
@@ -5,52 +5,46 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-public class BlockTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BlockTest {
 
     @Test
     void testIfTerminalBlocksForShortLine() throws IOException, InterruptedException {
-        SyncTerminalHandler.exec(new String[] {
-                "cat",
-                getClass().getClassLoader().getResource("terminal/shortLine.txt").getPath()
-        });
+        this.performTestWithFile("terminal/shortLine.txt");
     }
     @Test
     void testIfTerminalBlocksForMediumLine() throws IOException, InterruptedException {
-        SyncTerminalHandler.exec(new String[] {
-                "cat",
-                getClass().getClassLoader().getResource("terminal/mediumLine.txt").getPath()
-        });
+        this.performTestWithFile("terminal/mediumLine.txt");
     }
 
     @Test
     void testIfTerminalBlocksForLongLine() throws IOException, InterruptedException {
-        SyncTerminalHandler.exec(new String[] {
-                "cat",
-                getClass().getClassLoader().getResource("terminal/longLine.txt").getPath()
-        });
+        this.performTestWithFile("terminal/longLine.txt");
     }
 
     @Test
     void testIfTerminalBlocksForExtremlyLongLine() throws IOException, InterruptedException {
-        SyncTerminalHandler.exec(new String[] {
-                "cat",
-                getClass().getClassLoader().getResource("terminal/extremelyLongLine.txt").getPath()
-        });
+        this.performTestWithFile("terminal/extremelyLongLine.txt");
     }
 
     @Test
     void testIfTerminalBlocksForLongFiles() throws IOException, InterruptedException {
-        SyncTerminalHandler.exec(new String[] {
-                "cat",
-                getClass().getClassLoader().getResource("terminal/longFile.txt").getPath()
-        });
+        this.performTestWithFile("terminal/longFile.txt");
     }
 
     @Test
     void testIfTerminalBlocksForOneLineJson() throws IOException, InterruptedException {
+        this.performTestWithFile("terminal/oneLineJson.json");
+    }
+
+    void performTestWithFile(String filePath) throws IOException, InterruptedException {
         SyncTerminalHandler.exec(new String[] {
                 "cat",
-                getClass().getClassLoader().getResource("terminal/oneLineJson.json").getPath()
+                getClass().getClassLoader().getResource(filePath).getPath()
         });
+
+        // Asserting that it did not get stuck by trivially passing it after
+        assertTrue(true);
     }
 }
